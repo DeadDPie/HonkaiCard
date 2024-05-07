07152b08efa6b59bbba7fcc8281ff2dade611270
Subbotina Olesya <som@2i.tusur.ru>
2024-05-08 02:03:12 +0700

    добавление в избранное работает

--- app/src/main/java/com/example/honkaicard/ItemsAdapter.kt
+++ app/src/main/java/com/example/honkaicard/ItemsAdapter.kt
@@ -12,13 +12,14 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
-class ItemsAdapter(var items: List<Character>, var context: Context): RecyclerView.Adapter<ItemsAdapter.MyViewHolder>() {
-    class MyViewHolder(view: View): RecyclerView.ViewHolder(view){
+class ItemsAdapter(var items: List<Character>, var context: Context) :
+    RecyclerView.Adapter<ItemsAdapter.MyViewHolder>() {
+    class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val image: ImageView = view.findViewById(R.id.item_list_image)
         val icon: ImageView = view.findViewById(R.id.imageViewHe)
         val name: TextView = view.findViewById(R.id.item_list_title)
-        //val desc: TextView = view.findViewById(R.id.item_list_desc)
 
+        //val desc: TextView = view.findViewById(R.id.item_list_desc)
         val btn: Button = view.findViewById(R.id.item_list_button)
 
     }
@@ -38,42 +39,61 @@ class ItemsAdapter(var items: List<Character>, var context: Context): RecyclerVi
         //holder.desc.text = items[position].desc
 
 
-        val imageId = context.resources.getIdentifier(items[position].image,
-           "drawable",
-            context.packageName)
+        val imageId = context.resources.getIdentifier(
+            items[position].image,
+            "drawable",
+            context.packageName
+        )
         holder.image.setImageResource(imageId)
-var f = false
+
+        val chers = listOf(sampo, natasha)
+
+        // обновляем состояние кнопки в зависимости от значения поля fav
+
+
         holder.icon.setOnClickListener {
-            // изменяем значение поля fav у текущего элемента списка
-//            items[position].ffavor()
-//            f = !f
-//            items[position].ff = f
-//
-//            // обновляем состояние кнопки в зависимости от значения поля fav
-//            if (items[position].ff) {
-//                val iconId = context.resources.getIdentifier("h",
-//                    "drawable",
-//                    context.packageName)
-//                holder.icon.setImageResource(iconId)
-//            } else {
-//                val iconId = context.resources.getIdentifier("he",
-//                    "drawable",
-//                    context.packageName)
-//                holder.icon.setImageResource(iconId)
-//            }
+            Log.d("MyTag", items[position].id.toString())
+            for (cher in chers) {
+                if (items[position].id == cher.id) {
+                    cher.fav = !cher.fav
+                    items[position].fav = cher.fav
+                    notifyItemChanged(position)
+                    Log.d("MyTag", cher.fav.toString())
+                    Log.d("MyTag", items[position].fav.toString())
+                }
+            }
+        }
+        if (items[position].fav) {
+            val iconId = context.resources.getIdentifier(
+                "h",
+                "drawable",
+                context.packageName
+            )
+            holder.icon.setImageResource(iconId)
+
+            //items[position].fav = false
+        } else {
+            val iconId = context.resources.getIdentifier(
+                "he",
+                "drawable",
+                context.packageName
+            )
+            holder.icon.setImageResource(iconId)
+//                    items[position].fav = true
         }
 
-            val iconId = context.resources.getIdentifier("he",
-            "drawable",
-            context.packageName)
-        holder.icon.setImageResource(iconId)
+
+//            val iconId = context.resources.getIdentifier("he",
+//            "drawable",
+//            context.packageName)
+//        holder.icon.setImageResource(iconId)
 
         holder.btn.setOnClickListener {
             val intent = Intent(context, ItemActivity::class.java)
             //val db = DbHelper(this, null)
 
             //intent.putExtra("itemImage", items[position].image)
-            Log.d("MyTag", items[position].path.toString())
+            //Log.d("MyTag", items[position].path.toString())
             // Передача уникального ключа вместо имени файла изображения
             //intent.putExtra("itemKey", items[position].id)
             //intent.putExtra("imageResourceId", R.drawable.)
